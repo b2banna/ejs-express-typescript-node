09eb6f67baa9f7e6aec1e2e9251bee8aeda6ad5c
Krishan Kripal <krishankripal@outlook.com>
2022-02-25 09:16:28 +0530

    Added retrun for function

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,15 +2,15 @@ import { IUser, UserModel } from "../database/models/userModel";
 
 export class UserService {
 
-  public async getAllUsers(filter: any, sort: any) {
+  public async getAllUsers(filter: any, sort: any): Promise<Array<IUser>> {
     return await UserModel.find({ ...filter }).sort({ ...sort });
   }
 
-  public async getUserById(id: string) {
+  public async getUserById(id: string): Promise<IUser | null> {
     return await UserModel.findById(id);
   }
 
-  public async updateUserById(id: string, user: IUser) {
+  public async updateUserById(id: string, user: IUser): Promise<IUser | null> {
     return await UserModel.findByIdAndUpdate(id, user, { new: true });
   }
 }
