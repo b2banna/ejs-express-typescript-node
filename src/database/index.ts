c27175af55cf340b656db8ea1e488b8ecc54ff43
Krishan Kripal <krishankripal@outlook.com>
2022-02-25 19:04:10 +0530

    Comment added for kill process

--- src/database/index.ts
+++ src/database/index.ts
@@ -8,6 +8,7 @@ export class MongoDB {
       console.log(`\nConnect to the MongoDB database!`);
     } catch (error) {
       console.log(`\n[ERROR] Failed to the MongoDB database`, error);
+      // kill process if failed to connect to the database
       process.exit(1);
     }
   }
